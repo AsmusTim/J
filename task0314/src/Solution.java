1d30e4ab6599fb81a96f3d546ebb81be9caf67dd
Asmus Timofey <asmus.exceedteam@gmail.com>
2022-06-23 13:31:22 +0300

    task

--- task0314/src/Solution.java
+++ task0314/src/Solution.java
@@ -1,2 +1,10 @@
 public class Solution {
+    public static void main(String[] str){
+        for(int i = 0; i < 10; ++i){
+            for(int j = 0; j < 10; ++j){
+                System.out.print((i + 1)*(j + 1) + " ");
+            }
+            System.out.println();
+        }
+    }
 }
